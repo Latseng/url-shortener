baa01355aeb899897d20a137e948f72f384762eb
Lala Tseng <z162614@gmail.com>
2024-03-22 11:28:42 +0800

    build: add public folders & json for model

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//利用Node.js的file system建立json檔，儲存網址與產生的對應短網址
+const fs = require("fs");
+
+//產生短網址
+function generateShortUrl() {
+
+  saveUrl();
+}
+
+//儲存產生的短網址到json檔
+function saveUrl(data) {
+  fs.writeFile("data.json", '產生的短網址', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
 //產生器首頁
 app.get("/", (req, res) => {
   res.send("express app for shorterURL");
