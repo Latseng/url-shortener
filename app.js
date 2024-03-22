a773db2b8061286a047124503017946e1e703f84
Lala Tseng <z162614@gmail.com>
2024-03-22 11:04:23 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//產生器首頁
 app.get("/", (req, res) => {
-  res.send("express app for movies");
+  res.send("express app for shorterURL");
+});
+
+//導向原本URL
+app.get("/:short_URL", (req, res) => {
+  const short_URL = `generate_short_URL`
+  //先導向google以測試路由是否正常
+  const URL = `http://google.com`;
+  res.redirect(URL);
 });
 
 app.listen(port, () => {
